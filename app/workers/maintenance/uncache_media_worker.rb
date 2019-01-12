cbd834821616228c515ee3104f72a1b830f46c52
Vy-let <violet@violetbaddley.com>
2019-01-12 20:31:40 +0000

    runs media-removes on an extremely-low-priority maintenance queue

--- app/workers/maintenance/uncache_media_worker.rb
+++ app/workers/maintenance/uncache_media_worker.rb
@@ -3,7 +3,7 @@
 class Maintenance::UncacheMediaWorker
   include Sidekiq::Worker
 
-  sidekiq_options queue: 'pull'
+  sidekiq_options queue: 'maintenance'
 
   def perform(media_attachment_id)
     media = media_attachment_id.is_a?(MediaAttachment) ? media_attachment_id : MediaAttachment.find(media_attachment_id)
